2f22093607f851c00725d6a1c3865eb57de470ec
Wiz37 <davidwis616@gmail.com>
2026-07-31 11:03:18 -0400

    Assemble premium atlas C

---
+++ frontend/src/game/systems/PremiumAtlasC.ts
@@ -0,0 +1,8 @@
+import c0 from './PremiumAtlasC0';
+import c1 from './PremiumAtlasC1';
+import c2 from './PremiumAtlasC2';
+import c3 from './PremiumAtlasC3';
+import c4 from './PremiumAtlasC4';
+import c5 from './PremiumAtlasC5';
+import c6 from './PremiumAtlasC6';
+export default c0 + c1 + c2 + c3 + c4 + c5 + c6;
