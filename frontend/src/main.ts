2febb499bcd152ea1a17f9af03a4d5e21aef0146
Wiz37 <davidwis616@gmail.com>
2026-08-01 11:36:22 -0400

    Wire the finished corgi gameplay atlas into app startup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -13,6 +13,7 @@ import { installVisualPolish } from './game/systems/VisualPolishPlugin';
 import { installPirateCorgiFix } from './game/systems/PirateCorgiFixPlugin';
 import { installPirateTextureRepair } from './game/systems/PirateTextureRepairPlugin';
 import { installNewCorgiPack } from './game/systems/NewCorgiPackPlugin';
+import { installCorgiGameplayAnimation } from './game/systems/CorgiGameplayAnimationPlugin';
 import { HUDScene } from './game/scenes/HUDScene';
 import { PauseScene } from './game/scenes/PauseScene';
 import { GameOverScene } from './game/scenes/GameOverScene';
@@ -37,6 +38,7 @@ function boot() {
   installBonkEyes(GameScene);
   installVisualPolish(GameScene, MenuScene);
   installPirateCorgiFix(GameScene, CorgiSelectScene);
+  installCorgiGameplayAnimation(PreloadScene, GameScene);
   sound.init();
 
   const config: Phaser.Types.Core.GameConfig = {
